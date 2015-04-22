24a199a211ba63838d8322a3a0e689206ad745b6
Adam Coggeshall <dameerkat@gmail.com>
2015-04-22 16:14:01 -0600

    for some reason these did not commit.

--- lua/autorun/client/kil_test.lua
+++ lua/autorun/client/kil_test.lua
@@ -13,7 +13,7 @@ concommand.Add("kil_test",function()
 			local t = {}
 			t['a']=7
 			print(n,t.a)
-			
+
 			n=n+1
 		end
 	]]
@@ -24,4 +24,34 @@ concommand.Add("kil_test",function()
 	local testThread = killua.Thread(testClosure,function(err) print("OH NO:",err) end,10,20,30,40)
 	local _,num_ops = testThread:runT(1)
 	print("Executed "..num_ops.." ops in 1 second.")
+end)
+
+concommand.Add("kil_test2",function(ply)
+	//PrintTable(FindMetaTable("Player"))
+
+	local env = killua.Env(_G,{
+		Player=FindMetaTable("Player")
+	})
+
+	ply.asdf="ass"
+	print(">===>",ply.asdf)
+
+	local testCode = [[
+		local a = ...
+		print(a)
+		PrintTable(a:GetWeapons())
+
+		PrintTable{
+			eyy="lmao"
+		}
+
+		a.asdf = "dicks"
+
+
+	]]
+
+	testClosure = env:digest(testCode)
+	testClosure(ply)
+
+	print(">===>",ply.asdf)
 end)
